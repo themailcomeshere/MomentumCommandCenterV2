e969c37836e5d837813c4727989dad53bad32358
themailcomeshere <themailcomeshere@gmail.com>
2026-09-08 15:52:32 -0700

    Adding new projects/classes.

---
+++ MomentumCommandCenterV2.Application/Interfaces/ITradeImporter.cs
@@ -0,0 +1,8 @@
+﻿using MomentumCommandCenterV2.Application.Models;
+
+namespace MomentumCommandCenterV2.Application.Interfaces;
+
+public interface ITradeImporter
+{
+    TradeImportResult ImportFile(string filePath);
+}
---
+++ src/MomentumCommandCenterV2.Core/Models/TradeImportResult.cs
@@ -0,0 +1,11 @@
+﻿using MomentumCommandCenterV2.Core;
+using MomentumCommandCenterV2.Core.Models;
+
+namespace MomentumCommandCenterV2.Application.Models;
+
+public sealed record TradeImportResult(
+    IReadOnlyList<Trade> CompletedTrades,
+    IReadOnlyList<OpenPosition> OpenPositions,
+    IReadOnlyList<SchwabOrder> UnmatchedOrders,
+    int ImportedOrders,
+    IReadOnlyList<string> Errors);
