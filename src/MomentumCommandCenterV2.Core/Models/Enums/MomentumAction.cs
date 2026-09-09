00b697f7e91c46650b23094e6f757b9a05ad2433
themailcomeshere <themailcomeshere@gmail.com>
2026-09-09 15:35:36 -0700

    Added new enum

---
+++ src/MomentumCommandCenterV2.Core/Models/Enums/MomentumAction.cs
@@ -0,0 +1,12 @@
+﻿namespace MomentumCommandCenterV2.Core.Models.Enums
+{
+    public enum MomentumAction
+    {
+        NONE,
+        PREPARE_BUY,
+        BUY,
+        HOLD,
+        PREPARE_SELL,
+        SELL
+    }
+}
